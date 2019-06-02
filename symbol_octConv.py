e0fc244152fa2e27eb379cbd86f0f54f30c39674
雲夢 <h.zy@mail.scut.edu.cn>
2019-06-02 18:21:27 +0800

    Update symbol_octConv.py

--- symbol_octConv.py
+++ symbol_octConv.py
@@ -2,6 +2,9 @@ import mxnet as mx
 from symbol_basic import *
 
 def firstOctConv(data, settings, ch_in, ch_out, name, kernel=(1,1), pad=(0,0), stride=(1,1)):
+    ```
+    第一层OctConv，把输入分解为低频和高频
+    ```
     alpha_in, alpha_out = settings
     hf_ch_in = int(ch_in * (1 - alpha_in))
     hf_ch_out = int(ch_out * (1 - alpha_out))
@@ -17,11 +20,14 @@ def firstOctConv(data, settings, ch_in, ch_out, name, kernel=(1,1), pad=(0,0), s
     hf_pool = mx.symbol.Pooling(data=hf_data, pool_type='avg', kernel=(2,2), stride=(2,2), name=('%s_hf_pool' % name))
     hf_pool_conv = Conv(data=hf_pool, num_filter=lf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_hf_pool_conv' % name))
 
-    out_h = hf_conv 
-    out_l = hf_pool_conv 
+    out_h = hf_conv # 高频信号：原始信号-> 普通卷积（这里有疑问，为什么高频信号不应该是要减去低频信号才可以得到的吗）
+    out_l = hf_pool_conv # 低频信号：高频信号 -> 2x2 avg Pooling -> 卷积（有疑问，pooling后不是会变小很多吗）
     return out_h, out_l 
 
 def lastOctConv(hf_data, lf_data, settings, ch_in, ch_out, name, kernel=(1,1), pad=(0,0), stride=(1,1)):
+    ```
+    最后一层OctConv，把低频和高频合成为输入
+    ```
     alpha_in, alpha_out = settings
     hf_ch_in = int(ch_in * (1 - alpha_in))
     hf_ch_out = int(ch_out * (1 - alpha_out))
@@ -31,7 +37,7 @@ def lastOctConv(hf_data, lf_data, settings, ch_in, ch_out, name, kernel=(1,1), p
     hf_conv = Conv(data=hf_data, num_filter=hf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_hf_conv' % name))
 
     lf_conv = Conv(data=lf_data, num_filter=hf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_lf_conv' % name))
-    out_h = hf_conv + lf_conv
+    out_h = hf_conv + lf_conv # 低频高频分别卷积后相加合成结果信号
 
     return out_h 
 
@@ -50,10 +56,10 @@ def OctConv(hf_data, lf_data, settings, ch_in, ch_out, name, kernel=(1,1), pad=(
     hf_pool_conv = Conv(data=hf_pool, num_filter=lf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_hf_pool_conv' % name))
 
     lf_conv = Conv(data=lf_data, num_filter=hf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_lf_conv' % name))
-    if stride == (2, 2):
+    if stride == (2, 2):  # 如果大小需要减小，直接把低频卷积结果做pooling（毕竟如果要减少的话，高频也已经stride过了）
         lf_upsample = lf_conv
         lf_down = mx.symbol.Pooling(data=lf_data, pool_type='avg', kernel=(2,2), stride=(2,2), name=('%s_lf_down' % name))
-    else:
+    else: # 如果大小不变，直接UpSampling扩大低频特征图大小
         lf_upsample = mx.symbol.UpSampling(lf_conv, scale=2, sample_type='nearest',num_args=1, name='%s_lf_upsample' % name)
         lf_down = lf_data
     lf_down_conv = Conv(data=lf_down, num_filter=lf_ch_out, kernel=kernel, pad=pad, stride=(1,1), name=('%s_lf_down_conv' % name))
@@ -110,7 +116,7 @@ def Residual_Unit_norm(data, num_in, num_mid, num_out, name, first_block=False, 
     if first_block:
         data = Conv_BN( data=data, num_filter=num_out, kernel=( 1,  1), pad=( 0,  0), name=('%s_conv-w1' % name), stride=stride)
 
-    outputs = mx.symbol.ElementWiseSum(*[data, conv_m3], name=('%s_sum' % name))
+    outputs = mx.symbol.ElementWiseSum(*[data, conv_m3], name=('%s_sum' % name)) # 注意到，在这里的Residual block中的残差连接是以element wise product的形式进行的
     return AC(outputs)
 
 
